464fe7d4fe80fbb1f2755b90b702533080a5225a
Victor Hernandez <victormanu968@gmail.com>
2024-05-22 14:29:42 -0500

    commit inicial

---
+++ src/test/java/com/victor/proyectofinal/proyectodesarrolloback/ProyectoDesarrolloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.proyectofinal.proyectodesarrolloback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDesarrolloBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
